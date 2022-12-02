162d032ef5b8e44a60f1c0691f3ee335216f1220
Jefferson Leonata <leonatajeff@gmail.com>
2022-12-01 22:26:34 -0500

    Update content

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,12 @@ function App() {
   const projects = [
     {
       year: 2022,
+      title: "Colab at Pitt",
+      tags: ["Design"],
+      link: "https://www.figma.com/file/R4QlOWJieaAns7aEqYJhOB/2022-%C2%B7-Colab?t=KUnbaPq4R7km6EcD-1"
+    },
+    {
+      year: 2022,
       title: "OpenAI with Spotify",
       tags: ["Design", "Engineering"],
       link: "https://www.figma.com/file/spSPLPkPpgse7bZzOhqkgG/2022-%C2%B7-OpenAI-with-Spotify?node-id=0%3A1&t=MUeiGUv7JISosUh7-1"
@@ -25,12 +31,14 @@ function App() {
 
   return (
     <div className="App">
-      <button class="border-solid border bg-opacity-10 border-gray-300 bg-neutral-50 rounded-full outline-4 px-4 py-2 inline-flex items-center">
-        <span className="text-slate-50 font-semibold mr-3">
-          Now · Starting a Design Career
-        </span>
-        <ArrowRightIcon className="h-5 w-5 text-slate-50" />
-      </button>
+      <a className="btn">
+        <a href="https://www.linkedin.com/in/leonatajeff/" className="border-solid border bg-opacity-10 border-gray-300 bg-neutral-50 rounded-full outline-4 px-4 py-2 inline-flex items-center">
+          <span className="text-slate-50 font-semibold mr-3">
+            Now · Looking for a new grad role in design or engineering
+          </span>
+          <ArrowRightIcon className="h-5 w-5 text-slate-50" />
+        </a>
+      </a>
       <h1 className="font-bold m-6">Jefferson Leonata</h1>
       <div>
         {projects.map((value, index) => {
